ba7a0df1d19a75093a44d10e9ef35d5150717e42
MegaBeedrillTAO <jakekremling@yahoo.com>
2019-09-13 10:57:27 -0500

    Taking a quick break

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const controller = require('./controller');
+const massive = require('massive');
+require('dotenv').config();
+const {PORT_SERVER, CONNECTION_STRING, SESSION_SECRET} = process.env;
+
+massive(CONNECTION_STRING).then(dbInst => {
+    app.set('db', dbInst);
+    console.log("Connected to database.");
+})
 
 app.use(express.json());
 
-app.listen(4000, () => console.log("Listening dude"))
+app.listen(PORT_SERVER, () => console.log("Listening dude"))
--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-
+import {HashRouter} from 'react-router-dom';
 import './App.css';
-import Dashboard from './Components/Dashboard/Dashboard';
+import routes from './routes';
 import Header from './Components/Header/Header';
-import House from './Components/House/House';
-import Wizard from './Components/Wizard/Wizard';
+//import House from './Components/House/House';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Dashboard/>
-      <House/>
-      <Wizard/>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Header/>
+        {routes}
+        
+      </div>
+    </HashRouter>
   );
 }
 
