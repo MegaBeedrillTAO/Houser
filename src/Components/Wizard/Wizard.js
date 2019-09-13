c61642dccd264305a239550fba564086a7aa90f9
MegaBeedrillTAO <jakekremling@yahoo.com>
2019-09-13 10:18:33 -0500

    Got the basic server set up.

---
+++ src/Components/Wizard/Wizard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Wizard extends Component{
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Wizard;
